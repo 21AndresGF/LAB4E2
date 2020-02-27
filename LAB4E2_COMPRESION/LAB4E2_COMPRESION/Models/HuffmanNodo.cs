c8e217a505741dc67b6dfe8b8380dc1706864314
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-27 16:27:19 -0600

    huffman tree and huffman Nodo

---
+++ LAB4E2_COMPRESION/LAB4E2_COMPRESION/Models/HuffmanNodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB4E2_COMPRESION.Models
+{
+    public class HuffmanNodo
+    {
+        public HuffmanNodo NodoDerecho { get; set; }
+        public HuffmanNodo NodoIzquierdo { get; set; }
+        public char value { get; set; }
+        public float frecuencia { get; set; }
+    }
+}
--- LAB4E2_COMPRESION/LAB4E2_COMPRESION/Models/HuffmanTree.cs
+++ LAB4E2_COMPRESION/LAB4E2_COMPRESION/Models/HuffmanTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,43 @@ namespace LAB4E2_COMPRESION.Models
 {
     public class HuffmanTree
     {
-       
+        Dictionary<char, int> HuffmanDictionary = new Dictionary<char, int>();
+        List<HuffmanNodo> ModeloHuffman = new List<HuffmanNodo>();
+        public void Lectura()
+        {
+            string fichero = "";
+            string contenido = string.Empty;
+            try
+            {
+                using (StreamReader lector = new StreamReader(fichero))
+                {
+                    while (lector.Peek()>-1)
+                    {
+                        string linea = lector.ReadLine();
+                        if (!String.IsNullOrEmpty(linea))
+                        {
+                            
+                            for (int i = 0; i < linea.Length; i++)
+                                
+                                for (int j = 0; j < HuffmanDictionary.Count; j++)
+                                {
+                                    if (HuffmanDictionary.TryGetValue(linea[i], out int contador))
+                                    {
+                                        contador++;
+                                    }
+                                    else
+                                    {
+                                        HuffmanDictionary.Add(linea[i], 1);
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
