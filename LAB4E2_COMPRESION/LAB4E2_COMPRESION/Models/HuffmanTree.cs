243d5527f4dcd0654fcbcba96b961a1203a007f5
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-02-27 12:08:17 -0600

    huffman tree

---
+++ LAB4E2_COMPRESION/LAB4E2_COMPRESION/Models/HuffmanTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB4E2_COMPRESION.Models
+{
+    public class HuffmanTree
+    {
+       
+    }
+}
---
+++ LAB4E2_COMPRESION/LAB4E2_COMPRESION/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LAB4E2_COMPRESION
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
